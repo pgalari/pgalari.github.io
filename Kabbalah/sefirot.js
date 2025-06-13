ae4457a0bf14524c4743f532a6cd6083f45180bd
Api <62700063+pgalari@users.noreply.github.com>
2025-06-13 10:38:06 +0100

    Update sefirot.js
    Corregidas alineaciones

--- Kabbalah/sefirot.js
+++ Kabbalah/sefirot.js
@@ -1,13 +1,13 @@
 export const sefirot = [
-  { id: "keter", nombre: "Kéter", orden: 1, x: 300, y: 100, color: "violeta", descripcion: "Corona divina." },
+  { id: "keter", nombre: "Kéter", orden: 1, x: 350, y: 100, color: "violeta", descripcion: "Corona divina." },
   { id: "chochmah", nombre: "Jojmá", orden: 2, x: 200, y: 200, color: "azul", descripcion: "Sabiduría." },
-  { id: "binah", nombre: "Biná", orden: 3, x: 400, y: 200, color: "rojo", descripcion: "Entendimiento." },
-  { id: "daat", nombre: "Daat", orden: 4, x: 300, y: 250, color: "gris", descripcion: "Conocimiento." },
-  { id: "chesed", nombre: "Jésed", orden: 5, x: 150, y: 320, color: "celeste", descripcion: "Amor." },
-  { id: "gevurah", nombre: "Gevurá", orden: 6, x: 450, y: 320, color: "granate", descripcion: "Fuerza." },
-  { id: "tiferet", nombre: "Tiféret", orden: 7, x: 300, y: 400, color: "dorado", descripcion: "Belleza." },
+  { id: "binah", nombre: "Biná", orden: 3, x: 500, y: 200, color: "rojo", descripcion: "Entendimiento." },
+  { id: "daat", nombre: "Daat", orden: 4, x: 350, y: 250, color: "gris", descripcion: "Conocimiento." },
+  { id: "chesed", nombre: "Jésed", orden: 5, x: 200, y: 320, color: "celeste", descripcion: "Amor." },
+  { id: "gevurah", nombre: "Gevurá", orden: 6, x: 500, y: 320, color: "granate", descripcion: "Fuerza." },
+  { id: "tiferet", nombre: "Tiféret", orden: 7, x: 350, y: 400, color: "dorado", descripcion: "Belleza." },
   { id: "netzach", nombre: "Nétzaj", orden: 8, x: 200, y: 500, color: "verde", descripcion: "Victoria." },
-  { id: "hod", nombre: "Hod", orden: 9, x: 400, y: 500, color: "naranja", descripcion: "Gloria." },
-  { id: "yesod", nombre: "Yesod", orden: 10, x: 300, y: 600, color: "plata", descripcion: "Fundamento." },
-  { id: "malkuth", nombre: "Malkut", orden: 11, x: 300, y: 700, color: "tierra", descripcion: "Reino." }
+  { id: "hod", nombre: "Hod", orden: 9, x: 500, y: 500, color: "naranja", descripcion: "Gloria." },
+  { id: "yesod", nombre: "Yesod", orden: 10, x: 350, y: 600, color: "plata", descripcion: "Fundamento." },
+  { id: "malkuth", nombre: "Malkut", orden: 11, x: 350, y: 700, color: "tierra", descripcion: "Reino." }
 ];

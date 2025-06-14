a5e75adf687eab4de6f62bbe7949eacad6e48470
Api <62700063+pgalari@users.noreply.github.com>
2025-06-14 14:10:05 +0100

    Update orbs.js

--- MasterKey/orbs.js
+++ MasterKey/orbs.js
@@ -1,14 +1,14 @@
 export const orbs = [
-  { id: "keter", nombre: "Gama", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Corona divina." },
-  { id: "chochmah", nombre: "Beta", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Sabiduría." },
-  { id: "daat", nombre: "Theta", orden: 3, x: 350, y: 300, color: "green", descripcion: "Conocimiento." },
-  { id: "tiferet", nombre: "Alpha", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Belleza." },
-  { id: "yesod", nombre: "Delta", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Fundamento." },
-  { id: "malkuth", nombre: "Beta", orden: 6, x: 350, y: 600, color: "red", descripcion: "Reino." },
-  { id: "netzach", nombre: "Nétzaj", orden: 12, x: 250, y: 250, color: "pink", descripcion: "Victoria." },
-  { id: "netzach", nombre: "Nétzaj", orden: 10, x: 250, y: 350, color: "green", descripcion: "Victoria." },
-   { id: "chesed", nombre: "Jésed", orden: 8, x: 250, y: 450, color: "yellow", descripcion: "Relaciones" },
-  { id: "gevurah", nombre: "Gevurá", orden: 11, x: 250, y: 450, color: "violet", descripcion: "Fuerza." },
-  { id: "hod", nombre: "Hod", orden: 9, x: 450, y: 350, color: "lightblue", descripcion: "Gloria." },
-  { id: "binah", nombre: "Biná", orden: 7, x: 450, y: 450, color: "red", descripcion: "Servicio" }
+  { id: "orb-1", nombre: "Gama", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Vibración" },
+  { id: "orb-2", nombre: "Beta", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Química" },
+  { id: "orb-3", nombre: "Alpha", orden: 3, x: 350, y: 300, color: "green", descripcion: "Emociones." },
+  { id: "orb-4", nombre: "Theta", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Infomación" },
+  { id: "orb-5", nombre: "Delta", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Comunicación" },
+  { id: "orb-6", nombre: "2 Beta", orden: 6, x: 350, y: 600, color: "red", descripcion: "Interacción" },
+  { id: "orb-12", nombre: "Nétzaj", orden: 12, x: 250, y: 250, color: "pink", descripcion: "Familia" },
+  { id: "orb-10", nombre: "Nétzaj", orden: 10, x: 250, y: 350, color: "green", descripcion: "Pareja" },
+   { id: "orb-8", nombre: "Jésed", orden: 8, x: 250, y: 450, color: "yellow", descripcion: "Relaciones" },
+  { id: "orb-11", nombre: "Gevurá", orden: 11, x: 250, y: 450, color: "violet", descripcion: "Propósito" },
+  { id: "orb-9", nombre: "Hod", orden: 9, x: 450, y: 350, color: "lightblue", descripcion: "Habilidades." },
+  { id: "orb-7", nombre: "Biná", orden: 7, x: 450, y: 450, color: "red", descripcion: "Servicio" }
 ];

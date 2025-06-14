b5d32d974259c94aaa99560cc6d6ab6c02c08336
Api <62700063+pgalari@users.noreply.github.com>
2025-06-14 12:59:09 +0100

    Update sefirot.js

--- MasterKey/sefirot.js
+++ MasterKey/sefirot.js
@@ -1,13 +1,14 @@
 export const sefirot = [
-  { id: "keter", nombre: "Kéter", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Corona divina." },
-  { id: "chochmah", nombre: "Jojmá", orden: 2, x: 200, y: 200, color: "blue", descripcion: "Sabiduría." },
-  { id: "binah", nombre: "Biná", orden: 3, x: 500, y: 200, color: "red", descripcion: "Entendimiento." },
-  { id: "daat", nombre: "Daat", orden: 4, x: 350, y: 250, color: "gray", descripcion: "Conocimiento." },
-  { id: "chesed", nombre: "Jésed", orden: 5, x: 200, y: 320, color: "lightblue", descripcion: "Amor." },
-  { id: "gevurah", nombre: "Gevurá", orden: 6, x: 500, y: 320, color: "darkred", descripcion: "Fuerza." },
-  { id: "tiferet", nombre: "Tiféret", orden: 7, x: 350, y: 400, color: "gold", descripcion: "Belleza." },
-  { id: "netzach", nombre: "Nétzaj", orden: 8, x: 200, y: 500, color: "green", descripcion: "Victoria." },
-  { id: "hod", nombre: "Hod", orden: 9, x: 500, y: 500, color: "orange", descripcion: "Gloria." },
-  { id: "yesod", nombre: "Yesod", orden: 10, x: 350, y: 600, color: "silver", descripcion: "Fundamento." },
-  { id: "malkuth", nombre: "Malkut", orden: 11, x: 350, y: 700, color: "brown", descripcion: "Reino." }
+  { id: "keter", nombre: "Gama", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Corona divina." },
+  { id: "chochmah", nombre: "Beta", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Sabiduría." },
+  { id: "daat", nombre: "Theta", orden: 3, x: 350, y: 300, color: "green", descripcion: "Conocimiento." },
+  { id: "tiferet", nombre: "Alpha", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Belleza." },
+  { id: "yesod", nombre: "Delta", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Fundamento." },
+  { id: "malkuth", nombre: "Beta", orden: 6, x: 350, y: 600, color: "red", descripcion: "Reino." },
+  { id: "netzach", nombre: "Nétzaj", orden: 7, x: 200, y: 450, color: "pink", descripcion: "Victoria." },
+  { id: "netzach", nombre: "Nétzaj", orden: 8, x: 200, y: 350, color: "green", descripcion: "Victoria." },
+   { id: "chesed", nombre: "Jésed", orden: 9, x: 200, y: 250, color: "yellow", descripcion: "Amor." },
+  { id: "gevurah", nombre: "Gevurá", orden: 10, x: 500, y: 450, color: "violet", descripcion: "Fuerza." },
+  { id: "hod", nombre: "Hod", orden: 11, x: 500, y: 350, color: "lightblue", descripcion: "Gloria." },
+  { id: "binah", nombre: "Biná", orden: 12, x: 500, y: 250, color: "red", descripcion: "Entendimiento." }
 ];

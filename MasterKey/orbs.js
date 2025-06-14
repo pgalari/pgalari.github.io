0edb59edd81b03d0e77d3442c486a041548f0fff
Api <62700063+pgalari@users.noreply.github.com>
2025-06-14 13:37:07 +0100

    Update orbs.js

--- MasterKey/orbs.js
+++ MasterKey/orbs.js
@@ -5,10 +5,10 @@ export const orbs = [
   { id: "tiferet", nombre: "Alpha", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Belleza." },
   { id: "yesod", nombre: "Delta", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Fundamento." },
   { id: "malkuth", nombre: "Beta", orden: 6, x: 350, y: 600, color: "red", descripcion: "Reino." },
-  { id: "netzach", nombre: "Nétzaj", orden: 7, x: 200, y: 450, color: "pink", descripcion: "Victoria." },
-  { id: "netzach", nombre: "Nétzaj", orden: 8, x: 200, y: 350, color: "green", descripcion: "Victoria." },
-   { id: "chesed", nombre: "Jésed", orden: 9, x: 200, y: 250, color: "yellow", descripcion: "Amor." },
-  { id: "gevurah", nombre: "Gevurá", orden: 10, x: 500, y: 450, color: "violet", descripcion: "Fuerza." },
-  { id: "hod", nombre: "Hod", orden: 11, x: 500, y: 350, color: "lightblue", descripcion: "Gloria." },
-  { id: "binah", nombre: "Biná", orden: 12, x: 500, y: 250, color: "red", descripcion: "Entendimiento." }
+  { id: "netzach", nombre: "Nétzaj", orden: 7, x: 250, y: 450, color: "pink", descripcion: "Victoria." },
+  { id: "netzach", nombre: "Nétzaj", orden: 8, x: 250, y: 350, color: "green", descripcion: "Victoria." },
+   { id: "chesed", nombre: "Jésed", orden: 9, x: 250, y: 250, color: "yellow", descripcion: "Amor." },
+  { id: "gevurah", nombre: "Gevurá", orden: 10, x: 450, y: 450, color: "violet", descripcion: "Fuerza." },
+  { id: "hod", nombre: "Hod", orden: 11, x: 450, y: 350, color: "lightblue", descripcion: "Gloria." },
+  { id: "binah", nombre: "Biná", orden: 12, x: 450, y: 250, color: "red", descripcion: "Entendimiento." }
 ];

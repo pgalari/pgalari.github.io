09807a5bd7ce3b43124cba0ea94d6ba722af2f28
Api <62700063+pgalari@users.noreply.github.com>
2025-06-14 15:10:50 +0100

    Update orbs.js

--- MasterKey/orbs.js
+++ MasterKey/orbs.js
@@ -1,14 +1,14 @@
 export const orbs = [
-  { id: "orb-1", nombre: "Gama", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Vibración" },
-  { id: "orb-2", nombre: "Beta", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Química" },
-  { id: "orb-3", nombre: "Alpha", orden: 3, x: 350, y: 300, color: "green", descripcion: "Emociones." },
-  { id: "orb-4", nombre: "Theta", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Infomación" },
-  { id: "orb-5", nombre: "Delta", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Comunicación" },
-  { id: "orb-6", nombre: "2 Beta", orden: 6, x: 350, y: 600, color: "red", descripcion: "Interacción" },
-  { id: "orb-12", nombre: "Nétzaj", orden: 12, x: 250, y: 250, color: "pink", descripcion: "Familia" },
-  { id: "orb-10", nombre: "Nétzaj", orden: 10, x: 250, y: 350, color: "green", descripcion: "Pareja" },
-  { id: "orb-8", nombre: "Jésed", orden: 8, x: 250, y: 450, color: "yellow", descripcion: "Relaciones" },
-  { id: "orb-11", nombre: "Gevurá", orden: 11, x: 450, y: 250, color: "violet", descripcion: "Propósito" },
-  { id: "orb-9", nombre: "Hod", orden: 9, x: 450, y: 350, color: "lightblue", descripcion: "Habilidades." },
-  { id: "orb-7", nombre: "Biná", orden: 7, x: 450, y: 450, color: "red", descripcion: "Servicio" }
+  { id: "orb-1", nombre: "Orb 1", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Gama. Vibración" },
+  { id: "orb-2", nombre: "Orb 2", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Beta. Química" },
+  { id: "orb-3", nombre: "Orb 3", orden: 3, x: 350, y: 300, color: "green", descripcion: "Alpha. Emociones." },
+  { id: "orb-4", nombre: "Orb 4", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Theta. Infomación" },
+  { id: "orb-5", nombre: "Orb 5", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Delta. Comunicación" },
+  { id: "orb-6", nombre: "Orb 6", orden: 6, x: 350, y: 600, color: "red", descripcion: "Segundo Beta. Interacción" },
+  { id: "orb-12", nombre: "Orb 12", orden: 12, x: 250, y: 250, color: "pink", descripcion: "Familia" },
+  { id: "orb-10", nombre: "Orb 10", orden: 10, x: 250, y: 350, color: "green", descripcion: "Pareja" },
+  { id: "orb-8", nombre: "Orb 8", orden: 8, x: 250, y: 450, color: "yellow", descripcion: "Relaciones" },
+  { id: "orb-11", nombre: "Orb 11", orden: 11, x: 450, y: 250, color: "violet", descripcion: "Propósito" },
+  { id: "orb-9", nombre: "Orb 9", orden: 9, x: 450, y: 350, color: "lightblue", descripcion: "Habilidades." },
+  { id: "orb-7", nombre: "Orb 7", orden: 7, x: 450, y: 450, color: "red", descripcion: "Servicio" }
 ];

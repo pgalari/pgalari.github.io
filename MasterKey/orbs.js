5239032104e6b62fb514cf169e4989589e92eb41
Api <62700063+pgalari@users.noreply.github.com>
2025-06-14 15:49:45 +0100

    Update orbs.js

--- MasterKey/orbs.js
+++ MasterKey/orbs.js
@@ -1,11 +1,11 @@
 export const orbs = [
-  { id: "orb-1", nombre: "Orb 1", orden: 1, x: 350, y: 100, color: "violet", descripcion: "Gama. Vibración" },
+  { id: "orb-1", nombre: "Orb 1", orden: 1, x: 350, y: 100, color: "#7F00FF", descripcion: "Gama. Vibración" },
   { id: "orb-2", nombre: "Orb 2", orden: 2, x: 350, y: 200, color: "blue", descripcion: "Beta. Química" },
   { id: "orb-3", nombre: "Orb 3", orden: 3, x: 350, y: 300, color: "green", descripcion: "Alpha. Emociones." },
   { id: "orb-4", nombre: "Orb 4", orden: 4, x: 350, y: 400, color: "gold", descripcion: "Theta. Infomación" },
   { id: "orb-5", nombre: "Orb 5", orden: 5, x: 350, y: 500, color: "orange", descripcion: "Delta. Comunicación" },
   { id: "orb-6", nombre: "Orb 6", orden: 6, x: 350, y: 600, color: "red", descripcion: "Segundo Beta. Interacción" },
-  { id: "orb-12", nombre: "Orb 12", orden: 12, x: 450, y: 250, color: "pink", descripcion: "Familia" },
+  { id: "orb-12", nombre: "Orb 12", orden: 12, x: 450, y: 250, color: "#7F00FF", descripcion: "Familia" },
   { id: "orb-10", nombre: "Orb 10", orden: 10, x: 450, y: 350, color: "green", descripcion: "Pareja" },
   { id: "orb-8", nombre: "Orb 8", orden: 8, x: 450, y: 450, color: "gold", descripcion: "Relaciones" },
   { id: "orb-11", nombre: "Orb 11", orden: 11, x: 250, y: 250, color: "blue", descripcion: "Propósito" },

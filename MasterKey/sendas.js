47ac80228924084a36b73c9a97a7c970e2e93bca
Api <62700063+pgalari@users.noreply.github.com>
2025-06-15 15:35:28 +0100

    Update sendas.js

--- MasterKey/sendas.js
+++ MasterKey/sendas.js
@@ -26,5 +26,14 @@ export const sendas = [
   { de: "orb-7", a: "orb-9" },
   { de: "orb-8", a: "orb-10" },
   { de: "orb-9", a: "orb-11" },
-  { de: "orb-10", a: "orb-12" }
+  { de: "orb-10", a: "orb-12" },
+  { de: "orb-10", a: "orb-12" },
+  { de: "orb-7", a: "orb-3" },
+  { de: "orb-8", a: "orb-3" },
+  { de: "orb-9", a: "orb-2" },
+  { de: "orb-10", a: "orb-2" },
+  { de: "orb-11", a: "orb-4" },
+  { de: "orb-12", a: "orb-4" },
+   { de: "orb-9", a: "orb-5" },
+  { de: "orb-10", a: "orb-5" }
 ];

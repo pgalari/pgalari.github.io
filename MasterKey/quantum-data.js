d39dc132cdd0e9342e4ac65006d21bb4e1f0b59b
Api <62700063+pgalari@users.noreply.github.com>
2025-06-18 11:59:15 +0100

    Add files via upload

---
+++ MasterKey/quantum-data.js
@@ -0,0 +1,111 @@
+  export const orbes = [
+  {
+    id: "orb-1", tipo: "central", x: 350, y: 100, color: "#902692",
+    nombre: "Conexión. Vibración",
+    descripcion: "Onda Gama",
+    descripcionBreve: "Es el sistema principal.",
+    descripcionAmpliada: "Es el sistema de memoria vibracional. Hace posible que los demás sistemas estén activos en el cuerpo..."
+  },
+  {
+    id: "orb-2", tipo: "central", x: 350, y: 200, color: "#0071bd",
+    nombre: "Química",
+    descripcion: "Partícula Beta",
+    descripcionBreve: "Es el sistema de memoria nutricional.",
+    descripcionAmpliada: "Hace posible la digestión, el balance hormonal, el equilibrio hidro-electrólitico y el pH..."
+  },
+  {
+    id: "orb-3", tipo: "central", x: 350, y: 300, color: "#02a550",
+    nombre: "Emociones",
+    descripcion: "Onda Alpha",
+    descripcionBreve: "Es el sistema de memoria emocional.",
+    descripcionAmpliada: "El sistema límbico convierte la química cerebral en vibración. Es un sistema intuitivo conectado a todo el cuerpo..."
+  },
+  {
+    id: "orb-4", tipo: "central", x: 350, y: 400, color: "#fef200",
+    nombre: "Información",
+    descripcion: "Partícula Theta",
+    descripcionBreve: "Es el sistema de creencias y valores.",
+    descripcionAmpliada: "La personalidad, la solución de problemas, la toma de decisiones. Problemas obsesivos o hipocondría..."
+  },
+  {
+    id: "orb-5", tipo: "central", x: 350, y: 500, color: "#f7941f",
+    nombre: "Comunicación",
+    descripcion: "Onda Delta",
+    descripcionBreve: "Sistema de memoria de comunicación.",
+    descripcionAmpliada: "Conecta la realidad interior con el mundo externo. Es el puente entre lo que sientes y el mundo 3D..."
+  },
+  {
+    id: "orb-6", tipo: "central", x: 350, y: 600, color: "#fb0202",
+    nombre: "Interacción",
+    descripcion: "Partícula Segundo Beta",
+    descripcionBreve: "Sistema de memoria muscular.",
+    descripcionAmpliada: "Regula la motricidad y los movimientos. Si no hay energía aquí, hay baja acción física, fatiga, etc."
+  },
+
+  // Orbs orbitales con posiciones
+  {
+    id: "orb-7", tipo: "orbital", x: 250, y: 450, color: "#fb0202",
+    nombre: "Servicio", descripcion: "Relación con el entorno"
+  },
+  {
+    id: "orb-8", tipo: "orbital", x: 450, y: 450, color: "#fef200",
+    nombre: "Relaciones", descripcion: "Conexión emocional"
+  },
+  {
+    id: "orb-9", tipo: "orbital", x: 250, y: 350, color: "#04adf0",
+    nombre: "Habilidades", descripcion: "Competencias"
+  },
+  {
+    id: "orb-10", tipo: "orbital", x: 450, y: 350, color: "#00ff01",
+    nombre: "Pareja", descripcion: "Vinculo íntimo"
+  },
+  {
+    id: "orb-11", tipo: "orbital", x: 250, y: 250, color: "#0800ff",
+    nombre: "Propósito", descripcion: "Dirección y misión"
+  },
+  {
+    id: "orb-12", tipo: "orbital", x: 450, y: 250, color: "#eb018e",
+    nombre: "Familia", descripcion: "Origen, pertenencia"
+  }
+];
+
+ export const conexiones= [
+  { de: "orb-1", a: "orb-2" },
+  { de: "orb-2", a: "orb-3" },
+  { de: "orb-3", a: "orb-4" },
+  { de: "orb-4", a: "orb-5" },
+  { de: "orb-5", a: "orb-6" },
+  { de: "orb-1", a: "orb-11" },
+  { de: "orb-1", a: "orb-12" },
+  { de: "orb-2", a: "orb-11" },
+  { de: "orb-2", a: "orb-12" },
+  { de: "orb-11", a: "orb-12" },
+  { de: "orb-3", a: "orb-11" },
+  { de: "orb-3", a: "orb-12" },
+  { de: "orb-3", a: "orb-9" },
+  { de: "orb-3", a: "orb-10" },
+  { de: "orb-9", a: "orb-10" },
+  { de: "orb-4", a: "orb-9" },
+  { de: "orb-4", a: "orb-10" },
+  { de: "orb-4", a: "orb-7" },
+  { de: "orb-4", a: "orb-8" },
+  { de: "orb-7", a: "orb-8" },
+  { de: "orb-5", a: "orb-7" },
+  { de: "orb-5", a: "orb-8" },
+  { de: "orb-6", a: "orb-7" },
+  { de: "orb-6", a: "orb-8" },
+  { de: "orb-7", a: "orb-9" },
+  { de: "orb-8", a: "orb-10" },
+  { de: "orb-9", a: "orb-11" },
+  { de: "orb-10", a: "orb-12" },
+  { de: "orb-10", a: "orb-12" },
+  { de: "orb-7", a: "orb-3" },
+  { de: "orb-8", a: "orb-3" },
+  { de: "orb-9", a: "orb-2" },
+  { de: "orb-10", a: "orb-2" },
+  { de: "orb-11", a: "orb-4" },
+  { de: "orb-12", a: "orb-4" },
+  { de: "orb-9", a: "orb-5" },
+  { de: "orb-10", a: "orb-5" }
+];
+

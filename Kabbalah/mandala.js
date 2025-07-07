dbc5d767d60b0b614c6e326375fdb9fc5959eec7
Api <62700063+pgalari@users.noreply.github.com>
2025-07-07 16:08:39 +0100

    Update mandala.js

--- Kabbalah/mandala.js
+++ Kabbalah/mandala.js
@@ -2,7 +2,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("mandala");
   const total = orbes.length;
-  const radius = 180; //minimo, maximo 300
+  const radius = 200; //minimo, maximo 300
   const centerX = container.offsetWidth / 2;
   const centerY = container.offsetHeight / 2;
 

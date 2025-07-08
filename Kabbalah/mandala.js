bb98614c2d39dffb66fb38a0a645366ab4b12771
Api <62700063+pgalari@users.noreply.github.com>
2025-07-08 09:39:14 +0100

    Update mandala.js

--- Kabbalah/mandala.js
+++ Kabbalah/mandala.js
@@ -7,18 +7,18 @@ window.addEventListener("DOMContentLoaded", () => {
 //  const centerX = container.offsetWidth / 2;
 //  const centerY = container.offsetHeight / 2;
 
-    var alto = container.offsetWidth;
-    var ancho = container.offsetHeight;
+    var ancho = container.offsetWidth;
+    var alto = container.offsetHeight;
   alert (ancho);
-    alert (alto);
+  alert (alto);
  // innerHeight y innerWidth
   const centerX = ancho / 2;
   const centerY = alto / 2;
 
   orbes.forEach((orbe, i) => {
     const angle = (2 * Math.PI * i) / total;
-    const x = centerX + radius * Math.cos(angle) - 33;
-    const y = centerY + radius * Math.sin(angle) - 33;
+    const x = centerX + radius * Math.cos(angle) - 30;
+    const y = centerY + radius * Math.sin(angle) - 30;
 
     const div = document.createElement("div");
     div.className = "orbe";

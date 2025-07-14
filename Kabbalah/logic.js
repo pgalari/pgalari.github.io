7cc93e2ab70cf30c16799df456a0ae95ea70d6c1
Api <62700063+pgalari@users.noreply.github.com>
2025-07-14 01:37:23 +0100

    Update logic.js

--- Kabbalah/logic.js
+++ Kabbalah/logic.js
@@ -62,7 +62,7 @@ orbes.forEach((orb) => {
       text.textContent = c.nombre;
       circle.appendChild(text);
       
-      svg.appendChild(circle);*//
+      svg.appendChild(circle);*/
 
    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
       text.setAttribute("x", orb.x);
